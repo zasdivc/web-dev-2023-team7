0b99d35e4664af92c98441477d56df80ceb8c107
yangyulu <yangyulu1114@163.com>
2023-04-14 00:09:42 -0700

    trackDetail

--- web-dev/src/music/track.js
+++ web-dev/src/music/track.js
@@ -7,16 +7,32 @@ function TrackDetailsScreen() {
     const [track, setTrack] = useState({});
     const fetchTrack = async () => {
         const response = await getTrack(id);
+        console.log("get track response");
+        console.log(JSON.stringify(response, null, 2));
         setTrack(response);
     };
     useEffect(() => {
+        console.log("fetching track");
         fetchTrack();
     }, []);
+    console.log("rendering track");
+    console.log(JSON.stringify(track, null, 2))
     return (
-        <div>
-            <h1>{track.name}</h1>
+        <div>{
+            track.album !== undefined && <div>
             <img src={track.album.images[1].url}/>
-            <pre>{JSON.stringify(track, null, 2)}</pre>
+                <h1>{track.name}</h1>
+                <h1>{track.album.album_type}</h1>
+                <h2>{track.album.artists[0].name}</h2>
+                <h2>{track.album.artists[0].id}</h2>
+                <h2>{track.album.name}</h2>
+                <h2>{track.album.release_date}</h2>
+                <h2>{track.album.total_tracks} songs</h2>
+                <h2>{track.album.id}</h2>
+                <h2>{Math.floor(track.duration_ms / 60000)} minutes {Math.floor((track.duration_ms % 60000) / 1000)} seconds</h2>
+                <pre>{JSON.stringify(track, null, 2)}</pre>
+            </div>
+        }
         </div>
     );
 }
